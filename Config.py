aec3977c6296fbae15a92f7c5c931b090cbfed63
caeken <gavin.lgc@gmail.com>
2017-11-21 15:16:04 +0800

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,6 +1,12 @@
 class Config(object):
     ACCOUNT = 'admin'
     PASSWORD = 'xf321'
+    
+    SHODAN_CONFIG = {
+        "api_key" : "D17aQU0QNYBLHFEQWbmfgEYxgG7z3j0k",
+        "query_list" : [['Gateway port:23',30],['huawei country:cn port:8443',30],['zte country:cn port:80',1],['zte country:cn port:8080',1],['tp-link country:cn port:8080',1]]
+    }
+    #,['username country:cn port:23',200]
 
 
 class ProductionConfig(Config):
